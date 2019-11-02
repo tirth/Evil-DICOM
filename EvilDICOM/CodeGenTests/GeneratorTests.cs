9cdb909195de3767cb2f74c282322d5c9588992b
Tirth <complaints@tirthpatel.com>
2019-11-02 05:04:05 -0400

    Generate tag dictionary with T4 template

---
+++ EvilDICOM/CodeGenTests/GeneratorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EvilDICOM.CodeGenerator;
+using Xunit;
+using Xunit.Abstractions;
+using static EvilDICOM.CodeGenerator.GeneratorInstance;
+
+namespace EvilDICOM.CodeGeneratorTests
+{
+    public class GeneratorTests
+    {
+        private ITestOutputHelper _output;
+
+        public GeneratorTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void TestTagDictionary()
+        {
+            var dictEntries = DicomDefinitionLoader.LoadCurrentDictionary()
+                .Where(d => !string.IsNullOrEmpty(d.Keyword))
+                .Take(5)
+                .ToList();
+
+            
+        }
+    }
+}
